66e7c5fa2a056b03bae0b3e53b060756f8eede24
JaaxBass <jaaxbass@gmail.com>
2023-01-11 21:52:22 +0100

    zadanie dzień 12

--- src/ChallengeApp/Employee.cs
+++ src/ChallengeApp/Employee.cs
@@ -5,8 +5,9 @@ namespace ChallengeApp
 {
     public class Employee
     {
-        //public string name;
         public List<double> grades = new List<double>();
+        
+
         public Employee(string name)
         {       
             this.Name = name;
@@ -17,6 +18,27 @@ namespace ChallengeApp
             this.grades.Add(grade);
         }   
 
+        public void AddGradeString(string rate)
+        {
+            var grade = rate switch
+            {
+                "1+" => 1.5,
+                "2+" => 2.5,
+                "3+" => 3.5,
+                "4+" => 4.5,
+                "5+" => 5.5,
+                "1-" => 0.75,
+                "2-" => 1.75,
+                "3-" => 2.75,
+                "4-" => 3.75,
+                "5-" => 4.75,
+                "1" or "2" or "3" or "4" or "5" => double.Parse(rate),
+                _ => throw new ArgumentException($"Inserted grade is out of range. Valid grade in range of (+/- 1 to 5)")
+
+            };
+            this.grades.Add(grade);
+            Console.WriteLine($"Grade {grade} was added.");
+        }
         public void AddGradeToStringIfDouble(string grade)
         {
             if(double.TryParse(grade, out double result) && result >=0 && result <=100)
@@ -50,11 +72,12 @@ public void AddNameCheckIsItDigit(string name)
                 Console.WriteLine($"The following name: {name} has digit. The name: {name} doesn't meet conditions.");
                 Console.WriteLine($"The result of operation equals: {nameCheck}");
                 Console.ResetColor();
-            break;
+                break;
             }  
            }
-             if(nameCheck)
-             {
+
+            if(nameCheck)
+            {
                 this.Name = name;
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"The following name: {name} meet the conditions. The name: {name} was added to the system.");
@@ -72,14 +95,27 @@ public void AddNameCheckIsItDigit(string name)
         result.High = double.MinValue;
         result.Low = double.MaxValue;
         foreach(var n in this.grades)
-           {
+        {   
             result.Low = Math.Min(result.Low, n);
             result.High = Math.Max(result.High, n);
             result.Average += n;    
-           }
-           result.Average /=grades.Count;
-           result.Difference = (result.High - result.Low);
-           return result;             
-    }  
+        }   
+        result.Average /=grades.Count;
+        result.Difference = (result.High - result.Low);
+        return result;             
+    }   
+
+    public string[] studentNames = new string[] {"Fryc", "Abelard", "Hans", "Żelisław", "Pankracy", "Bonifacy", "Uwe", "Adolf", "Eustachy", "Baltazar"};
+    public int [] studentAge = new int [] {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+
+    public void ForLoopTest()   
+    {
+        for (var index = 0; index < studentAge.Length; index++ )
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{studentNames[index]} is {studentAge[index]} years old");
+            Console.ResetColor();
+        }
+    }     
 }
 }
--- src/ChallengeApp/Program.cs
+++ src/ChallengeApp/Program.cs
@@ -7,40 +7,55 @@ class Program
     {
         
         static void Main(string[] args)
-        {  
-        Console.Clear(); 
+        { 
+        Console.Clear();      
         Console.WriteLine("Insert student name:");
         var employee = new Employee(Console.ReadLine());
+        
+        while(true)
+        {
+            Console.WriteLine($"Insert a grade for the student: {employee.Name}, or press q to exit.");
+            var input = Console.ReadLine();
 
-        //Console.WriteLine("Enter the time of first lap:");
-        //double Lap1time = Convert.ToDouble(Console.ReadLine());
-        //Console.WriteLine("Enter the time of second lap:");
-        //double Lap2time = Convert.ToDouble(Console.ReadLine());
-        //Console.WriteLine("Enter the time of third lap:");
-        //double Lap3time = Convert.ToDouble(Console.ReadLine());
-        //Console.WriteLine("Enter the time of fourth lap:");
-        //double Lap4time = Convert.ToDouble(Console.ReadLine());
-        //employee.AddGrade(Lap1time);  
-        //employee.AddGrade(Lap2time);
-        //employee.AddGrade(Lap3time);
-        //employee.AddGrade(Lap4time);
-        employee.AddGradeToStringIfDouble("21,37");
-        employee.AddGradeToStringIfDouble("12");
-        employee.AddGradeToStringIfDouble("-55");
-        employee.AddGradeToStringIfDouble("21");
-        employee.AddGradeToStringIfDouble("220");
+            if(input == "q")
+            {
+                break;
+            }
+            try
+            {
+                employee.AddGradeString(input);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
+        
+        var stat = employee.GetStatistics();
+
+        Console.WriteLine($"The student {employee.Name} has completed the task with the following grades:");
+        Console.WriteLine($"Student {employee.Name} average grades are: {stat.Average}s from {employee.grades.Count} Grades.");
+        Console.WriteLine($"Student {employee.Name} lowest grade is: {stat.Low}.");
+        Console.WriteLine($"Student {employee.Name} highest grade is: {stat.High}.");
+        Console.WriteLine($"Difference between highest and lowest grade is: {stat.Difference}.");
+        
+        //employee.AddGradeToStringIfDouble("0,0");
+        //employee.AddGradeToStringIfDouble("12");
+        //employee.AddGradeToStringIfDouble("-55");
+        //employee.AddGradeToStringIfDouble("21");
+        //employee.AddGradeToStringIfDouble("220");
+        
         employee.AddNameCheckIsItDigit("Maciek");
         employee.AddNameCheckIsItDigit("W0jt6k");
         employee.AddNameCheckIsItDigit("Radek");
         employee.AddNameCheckIsItDigit("Pi0tr");
-
-        var stat = employee.GetStatistics();
-
-        Console.WriteLine($"The student {employee.Name} has completed the track with the following results:");
-        Console.WriteLine($"Average track time: {stat.Average}s from {employee.grades.Count} Laps");
-        Console.WriteLine($"The best track time: {stat.Low}s");
-        Console.WriteLine($"The lowest track time: {stat.High}s");
-        Console.WriteLine($"Time difference : {stat.Difference}s");
+        employee.ForLoopTest(); 
         }
     }
  
